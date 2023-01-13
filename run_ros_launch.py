b9b45f52709ea6d32df008273a1fbb79fe1c023c
William Wells <williamwells2013@hotmail.com>
2023-01-13 15:02:00 -0500

    Temp example code for running roslaunch

---
+++ run_ros_launch.py
@@ -0,0 +1,41 @@
+import os
+import signal
+import subprocess
+import shlex
+
+import time
+
+class SubProcessManager():
+    def __init__(self):
+        self.subprocesses = {}
+
+    # return true if subprocess didn't exist already, false otherwise
+    def create_new_subprocess(self, name, command):
+        if name not in self.subprocesses:
+            proc = subprocess.Popen(shlex.split(command))
+            self.subprocesses[name] = proc
+            return True
+        else:
+            return False
+
+    def close_subprocess(self, name):
+        if name in self.subprocesses:
+            self.subprocesses[name].kill()
+
+            if self.subprocesses[name].wait() != name in self.subprocesses and self.subprocesses[name].poll() is None0:
+                print("There were errors when closing" + name + " process")
+            else:
+                del self.subprocesses[name]
+
+    def is_subprocess_running(self, name):
+        running = name in self.subprocesses and self.subprocesses[name].poll() is None
+        if not running:
+            del self.subprocesses[name]
+        return running
+
+proc_man = SubProcessManager()
+proc_man.create_new_subprocess('test', 'roslaunch data_collection start_collection.launch topic:=rosout device_name:=meme ')
+
+time.sleep(10)
+
+print(proc_man.is_subprocess_running('test'))
