600b3ac430ff4c1d9b568d11231e08f87d0b8661
MohamadAliTaha <mohamadalitaha03@gmail.com>
2023-04-25 16:57:42 -0400

    fixed cpp_monitor node

--- UI/scripts/cpp_monitor.cpp
+++ UI/scripts/cpp_monitor.cpp
@@ -5,22 +5,23 @@
 #include "std_msgs/String.h"
 #include "sensor_msgs/Imu.h"
 
-#define MAXJERK 4.0
-
 using namespace std;
 
 int main(int argc, char* argv[]) {
+    const double MAXJERK = 4.0;
+    double prevAcc{0};
+
     ros::init(argc, argv, "cpp_monitor");
     ros::NodeHandle nh;
     ros::Publisher pub = nh.advertise<const std_msgs::String>("robot_handler_cmd", 10);
-
-    double prevAcc{0};
 
     auto callback = [&] (const sensor_msgs::Imu::ConstPtr& data) {
         double x = data->linear_acceleration.x;
         double y = data->linear_acceleration.y;
         double acc = sqrt(pow(x, 2) + pow(y, 2));
         double jerk = acc - prevAcc;
+        
+        prevAcc = acc;
 
         if (jerk > MAXJERK) {
             std_msgs::String msg;
